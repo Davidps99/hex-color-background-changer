f43daaffac422157761647df47f38b7410bde029
David Pérez <devrap99@gmail.com>
2023-04-20 14:02:54 +0200

    ACABADO

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 // Implementar solución aquí
 
+function getRandomHexColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+document.querySelector("#btn").addEventListener("click", function () {
+  let hexColor = getRandomHexColor();
+  document.body.style.backgroundColor = hexColor;
+  document.querySelector("#hex-value").textContent = hexColor;
+  console.log("color aleatorio: ", getRandomHexColor());
+});
